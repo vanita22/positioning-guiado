8ec08ff96358d8ff7e884b070961efc8849f41d0
vanita22 <inesvanessapcm@gmail.com>
2017-05-22 12:29:22 -0400

    subiendo desafio 5

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
